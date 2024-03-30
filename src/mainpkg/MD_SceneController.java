79a88f23c148abcfeffd33dc47dca007cd053a65
arafa <arafa@LAPTOP-IOUJVFQ0>
2024-03-31 00:12:14 +0600

    Commit second

---
+++ src/mainpkg/MD_SceneController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author arafa
+ */
+public class MD_SceneController implements Initializable {
+
+    @FXML
+    private TextField getSoldCars;
+    @FXML
+    private TextField getImportedCars;
+    @FXML
+    private TextField getCarsOnThePort;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/mainpkg/UserSceneController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author arafa
+ */
+public class UserSceneController implements Initializable {
+
+    @FXML
+    private TextField getID;
+    @FXML
+    private TextField getPassword;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
